ed2ce76a57c4eea8dce7d70bee0e2d23d2a89559
joannaWebDev <joannasmerea@gmail.com>
2020-11-30 22:49:35 +0100

    node_modules test

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 
-// Add this in node_modules/react-dom/index.js
-window.React1 = require('react');
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
