71e138b5ee9b4761f0300bf823990c3aa7bfe946
anudeepayilalath <anpds816@gmail.com>
2020-12-01 14:11:28 +0100

    Social Media Comp issue

---
+++ client/src/components/SocialMedia/SocialMedia.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './SocialMedia.css';
+const SocialMedia = () =>{
+
+    return (
+        <>
+                <h3>Siguenos o comparte el proyecto</h3>
+                <div id="thirdItem">
+                    <i class="fab fa-instagram"></i>
+                    <img src="https://cdn0.iconfinder.com/data/icons/global-top-brands/90/facebook-logo-256.png" width="40px"/>
+                    <img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Twitter5_svg-256.png" width="40px"/>
+                    <img src="https://cdn0.iconfinder.com/data/icons/simple-darkcon-1/85/envelope-256.png" width="40px"/>
+                </div>
+                </>
+            
+      
+    )
+
+};
+
+export default SocialMedia;
